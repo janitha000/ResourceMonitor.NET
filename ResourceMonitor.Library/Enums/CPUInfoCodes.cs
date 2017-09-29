8107d20a15bc44fd306cce420f307dc00b0e2eeb
Janitha Tennakoon <janitha000@gmail.com>
2017-09-29 08:50:06 +0530

    Added services

---
+++ ResourceMonitor.Library/Enums/CPUInfoCodes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourceMonitor.Library.Enums
+{
+    class CPUInfoCodes
+    {
+        public const string BaseBoard = "Win32_BaseBoard";
+    }
+}
