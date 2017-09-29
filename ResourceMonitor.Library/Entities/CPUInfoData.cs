27bb21cdcc614ad4ebd47c262fd08c79fe3df31b
Janitha Tennakoon <janitha000@gmail.com>
2017-09-29 13:00:16 +0530

    #1 Added CPUInfoData

--- ResourceMonitor.Library/Entities/CPUInfoData.cs
+++ ResourceMonitor.Library/Entities/CPUInfoData.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ResourceMonitor.Library.Entities
+﻿namespace ResourceMonitor.Library.Entities
 {
     public class CPUInfoData
     {
