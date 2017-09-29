4987aab1e76388befd7f6c2cca632b0c565d2119
Janitha Tennakoon <janitha000@gmail.com>
2017-09-29 10:45:01 +0530

    Added simple query

---
+++ ResourceMonitor.Library/Services/CPUInfoService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace ResourceMonitor.Library.Services
+{
+    public class CPUInfoService
+    {
+        public string  GetCpuInfo()
+        {
+            ManagementObjectSearcher objMOS = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM  Win32_OperatingSystem");
+            return objMOS.ToString();
+        }
+    }
+}
---
+++ ResourceMonitor/Controllers/CPUController.cs
@@ -0,0 +1,34 @@
+﻿using ResourceMonitor.Library.Entities;
+using ResourceMonitor.Library.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ResourceMonitor.Controllers
+{
+    public class CPUController : ApiController
+    {
+        [HttpGet]
+        [Route("api/CPU/cpuinfo")]
+        public string GetCPUInfo()
+        {
+
+            try
+            {
+                CPUInfoService cpuInfo = new CPUInfoService();
+                string info = cpuInfo.GetCpuInfo();
+                return info;
+
+
+            }
+            catch (Exception ex)
+            {
+                
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.InternalServerError));
+            }
+        }
+    }
+}
