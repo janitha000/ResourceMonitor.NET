14603b5c2db2aed7f76b3e1dc851458fcedb38e1
Janitha Tennakoon <janitha000@gmail.com>
2017-09-29 08:29:40 +0530

    Added project files

---
+++ ResourceMonitor/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ResourceMonitor.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ResourceMonitor/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ResourceMonitor.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
