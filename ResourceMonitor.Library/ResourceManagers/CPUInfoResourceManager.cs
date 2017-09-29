79f7671db0ba43ef494ec1a7d7d7c13faa4fb740
Janitha Tennakoon <janitha000@gmail.com>
2017-09-29 14:52:28 +0530

    #1 added CPUInfoResourceManager

--- ResourceMonitor.Library/ResourceManagers/CPUInfoResourceManager.cs
+++ ResourceMonitor.Library/ResourceManagers/CPUInfoResourceManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ResourceMonitor.Library.Entities;
+using ResourceMonitor.Library.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,23 @@ namespace ResourceMonitor.Library.ResourceManagers
 {
     class CPUInfoResourceManager
     {
+        private CPUInfoService service = new CPUInfoService();
+
+        public CPUInfoData GetData(string type)
+        {
+            if(type == "basic")
+            {
+                return service.GetBasicInfo();
+            }
+            else if(type == "advanced")
+            {
+                return service.GetAdvancedInfo();
+            }
+            else
+            {
+                return null;
+            }
+
+        }
     }
 }
